24a348b2f301a601fcdce8bfaeabda21c1c83a81
ArjenR49 <54793526+ArjenR49@users.noreply.github.com>
2021-05-11 09:49:13 +0300

    Add files via upload

--- PowerCycle.py
+++ PowerCycle.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 # adapted from scripts provided at GitHub: Geeekpi/upsplus by nickfox-taterli
-# ar - 10-05-2021
+# ar - 11-05-2021
 
 # ''' Halt the Pi, power down, then power up Pi (= perform power cycle) '''
 
@@ -16,49 +16,54 @@ DEVICE_BUS = 1
 # Define device I2C slave address.
 DEVICE_ADDR = 0x17
 
-# Essential UPS I2C register default values (name format: Operation Mode Register Address)
-# for shut down & power off state
-OMR0x18=0   # seconds, power off delay
-OMR0x19=0   # boolean, automatic restart (1) or not (0)
-OMR0x1A=120  # seconds, power up delay
-
 # Raspberry Pi Communicates with MCU via I2C protocol.
 bus = smbus2.SMBus(DEVICE_BUS)
 
+# Essential UPS I2C register default values
+# (name format: Operation Mode Register Address)
+# for shut down & power off state
+OMR0x18 = 0    # seconds, power off delay
+OMR0x19 = 0    # boolean, automatic restart or not
+OMR0x1A = 240  # seconds, power up delay
+
+# Write byte to specified I2C register address
+def putByte(RA, byte):
+    while True:
+        try:
+            bus.write_byte_data(DEVICE_ADDR, RA, byte)
+            time.sleep(0.1)
+            break
+        except TimeoutError:
+            continue
+
 print("*"*62)
-print(("*** {:^54s} ***").format("Make the UPS sync & shut down Pi in an orderly manner"))
-print(("*** {:^54s} ***").format("without delay & turn power off after "+str(OMR0x1A)+" seconds."))
-print(("*** {:^54s} ***").format("Five seconds later the UPS will power up the Pi again."))
+print(("*** {:^54s} ***").
+      format("Make the UPS sync & shut down Pi in an orderly manner"))
+print(("*** {:^54s} ***").format("without delay & turn power off after "
+      + str(OMR0x1A)+" seconds."))
+print(("*** {:^54s} ***").
+      format("Five seconds later the UPS will power up the Pi again."))
 print("*"*62)
 print()
 print()
 
-#exit()
+# exit()
 
-# 1. Disable/enable automatic restart on return of external power.
-#    Enable: write 1 to register 0x19
-#    Disable: write 0 to register 0x19
-while True:
-    try:
-        bus.write_byte_data(DEVICE_ADDR, 0x19, OMR0x19)
-        time.sleep(0.1)
-        break
-    except TimeoutError:
-        continue
+# Disable/enable automatic restart on return of external power.
+#   Enable: write 1 to register 0x19
+#   Disable: write 0 to register 0x19
+putByte(0x19, OMR0x19)
 
-# For power cycle operation only 'power up' countdown register (0x1A) must be set.
-# UPS will cut power to the Pi 5 seconds before the end of the power up countdown,
-# allowing the Pi to sync & halt in an orderly manner,
+# For power cycle operation only 'power up' countdown (0x1A) must be set.
+# UPS will cut power to the Pi 5 seconds before the end of the power up
+# countdown, allowing the Pi to sync & halt in an orderly manner,
 # and then turn power to the Pi on again at 0 seconds.
-while True:
-    try:
-        bus.write_byte_data(DEVICE_ADDR, 0x1A, OMR0x1A)
-        time.sleep(0.1)
-        break
-    except TimeoutError:
-        continue
+putByte(0x1A, OMR0x1A)
 
-# Make Pi perform sync and then halt.
+# Unset power off delay timer
+putByte(0x18, OMR0x18)
+
+# Halt the Pi without delay.
 os.system("sudo shutdown now")
 
 # Script continues executing, indefinitely as it were (& keeping the lock)

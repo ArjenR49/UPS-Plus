017df2d21c4580676a119f41f85c47e3f205834f
ArjenR49 <54793526+ArjenR49@users.noreply.github.com>
2021-05-18 10:57:33 +0300

    Add files via upload

--- CatchExceptions3.py
+++ CatchExceptions3.py
@@ -29,13 +29,19 @@ RA = 0x18
 #         byte = gbus.read_byte_data(DEVICE_ADDR, RA)
 #     return [byte]
 
+def settle():
+    time.sleep(0.5)
+
 i = 0
 while True:
     try:
+        settle()
         with SMBus(DEVICE_BUS) as pbus:
             pbus.write_byte_data(DEVICE_ADDR, RA, 180)
+        settle()
         with SMBus(DEVICE_BUS) as gbus:
             byte = gbus.read_byte_data(DEVICE_ADDR, RA)
+        settle()
         if (byte in (180,179)):
             pass
             print('OK            ', i, ' - ', byte)
@@ -43,8 +49,10 @@ while True:
             print('read != write ', i, ' - ', byte, "NOT in (180,179)")
         with SMBus(DEVICE_BUS) as pbus:
             pbus.write_byte_data(DEVICE_ADDR, RA, 0)
+        settle()
         with SMBus(DEVICE_BUS) as gbus:
             byte = gbus.read_byte_data(DEVICE_ADDR, RA)
+        settle()
         if (byte == 0):
             pass
             print('OK            ', i, ' - ', byte)
@@ -54,7 +62,7 @@ while True:
         i += 1
     except TimeoutError as e:
         print(i, ' - ', byte, ' - ', e)
-        time.sleep(0.1)
+        settle()
     except KeyboardInterrupt:
         break
     except:

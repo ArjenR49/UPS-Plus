23fac6cefc1d38dc1d2361d1a0635230d91e8947
ArjenR49 <54793526+ArjenR49@users.noreply.github.com>
2022-06-09 00:14:35 +0200

    Add files via upload

--- reset2FactoryDefaults.py
+++ reset2FactoryDefaults.py
@@ -2,35 +2,46 @@
 # -*- coding: utf-8 -*-
 
 # adapted from scripts provided at GitHub: Geeekpi/upsplus by nickfox-taterli
-# Enable UPS function "0x1B Reset to Factory Defaults 0/1 Bool"
-# ar - 21-05-2021, 07-08-2021, 19-08-2021, 06-06-2022
+# Execute UPS function "0x1B Reset to Factory Defaults 0/1 Bool"
+# ar - 08-06-2022
 
-from smbus2 import SMBus
+import sys
+import smbus2
+import syslog
+import click
 
+# Define I2C bus
 DEVICE_BUS = 1
+
+# Define device i2c slave address.
 DEVICE_ADDR = 0x17
 
-RESET_FLAG=0
+# Raspberry Pi communicates with MCU via i2c protocol.
+bus = smbus2.SMBus(DEVICE_BUS)
 
-# Write byte to specified I2C register address 'until it sticks'.
-def putByte(RA, wbyte):
-    while True:
-        try:
-            with SMBus(DEVICE_BUS) as pbus:
-                pbus.write_byte_data(DEVICE_ADDR, RA, wbyte)
-            with SMBus(DEVICE_BUS) as gbus:
-                rbyte = gbus.read_byte_data(DEVICE_ADDR, RA)
-            if (wbyte) <= rbyte <= (wbyte):
-                print("OK ", wbyte, rbyte)
-                break
-            else:
-                raise ValueError
-        except ValueError:
-            print("Write:", wbyte, "!= Read:", rbyte, " Trying again")
-            pass
-        
-putByte(0x1B, RESET_FLAG & 0xFF)
+print("*"*62)
+print(("*** {:^54s} ***").format("-- Perform reset to factory defaults --"))
+print(("*** {:^54s} ***").format("By writing 1 to register 0x1B the script"))
+print(("*** {:^54s} ***").format("makes the f/w reset various settings."))
+print(("*** {:^54s} ***").format("Ultimately the register will be set back to 0."))
+print("*"*62)
+print()
 
-print("Register at 0x1B was set to: %d"  % RESET_FLAG)
+if click.confirm('Do you want to continue?', default=True):
+    click.echo('Resetting UPS to factory defaults ...')
+else:
+    print('Script aborted')
+    exit()
 
-#EOF
+#exit()
+
+try:
+    bus.write_byte_data(DEVICE_ADDR, 0x1B, 0x01)    
+    msg = "UPS Plus reset to factory defaults!"
+    print(msg)
+except Exception as e:
+    errmsg = "Error resetting UPS Plus to factory defaults: " + str(e)
+    print(errmsg)
+    syslog.syslog(syslog.LOG_ERR, errmsg)
+
+# EOF
